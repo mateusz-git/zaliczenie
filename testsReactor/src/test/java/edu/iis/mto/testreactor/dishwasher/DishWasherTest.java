a2efb872eb192774cac81db4f0941b07d8877853
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-06-09 17:37:00 +0200

    Created setUp method and mock variables

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -2,11 +2,33 @@ package edu.iis.mto.testreactor.dishwasher;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class DishWasherTest {
+    @Mock
+    private WaterPump waterPump;
+    @Mock
+    private Engine engine;
+    @Mock
+    private DirtFilter dirtFilter;
+    @Mock
+    private Door door;
 
+    private DishWasher dishWasher;
+    private ProgramConfiguration programConfiguration;
+
+    @BeforeEach
+    public void setUp(){
+        dishWasher = new DishWasher(waterPump,engine,dirtFilter,door);
+    }
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
