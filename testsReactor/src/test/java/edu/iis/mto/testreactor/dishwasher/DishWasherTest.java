5bf136426974614389a904006f473fcb9573c817
Pietruszewski Mateusz <215851@edu.p.lodz.pl>
2020-06-09 18:19:46 +0200

    Added test where should verify correct order

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,16 +1,20 @@
 package edu.iis.mto.testreactor.dishwasher;
 
 import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.engine.EngineException;
+import edu.iis.mto.testreactor.dishwasher.pump.PumpException;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +30,6 @@ class DishWasherTest {
 
     private DishWasher dishWasher;
     private ProgramConfiguration programConfiguration;
-    private WashingProgram washingProgram;
 
     @BeforeEach
     public void setUp() {
@@ -66,6 +69,7 @@ class DishWasherTest {
 
         assertEquals(runResult.getStatus(), Status.ERROR_FILTER);
     }
+
     @Test
     public void shouldRunProgramWithErrorDoorOpen() {
         when(door.closed()).thenReturn(false);
@@ -77,5 +81,22 @@ class DishWasherTest {
         RunResult runResult = dishWasher.start(programConfiguration);
 
         assertEquals(runResult.getStatus(), Status.DOOR_OPEN);
+    }
+
+    @Test
+    public void shouldVerifyCorrectOrder() throws PumpException, EngineException {
+        when(door.closed()).thenReturn(true);
+        when(dirtFilter.capacity()).thenReturn(100d);
+        programConfiguration = ProgramConfiguration.builder()
+                .withTabletsUsed(true)
+                .withFillLevel(FillLevel.FULL)
+                .withProgram(WashingProgram.RINSE)
+                .build();
+        dishWasher.start(programConfiguration);
+
+        InOrder callOrder = inOrder(waterPump, engine);
+        callOrder.verify(waterPump).pour(FillLevel.FULL);
+        callOrder.verify(engine).runProgram(WashingProgram.RINSE);
+        callOrder.verify(waterPump).drain();
     }
 }
